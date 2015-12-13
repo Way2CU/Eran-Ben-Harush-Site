371fa6454d7c0bec442973ddf4e1d7dc45814931
tal <talreznic11@gmail.com>
2015-12-13 22:12:08 +0200

    Script: Updated indentation

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -46,54 +46,54 @@ Site.is_mobile = function() {
 };
 
 function Tween(target, start_value, end_value, duration) {
-    var self = this;
-    
-    self._target = target;
-    self._start_value = start_value;
-    self._end_value = end_value;
-    self._duration = duration;
-    self._current_value = start_value;
-    self._total_value = null;
-    self._step_size = null;
-    self._last_frame_time = null;
-        
-    self._init = function() {
-        // pre-calculate values to minimize operations later
-        self._total_value = self._end_value - self._start_value;
-        self._step_size = self._total_value / self._duration;
-        
-        // initial frame time
-        //Here
-        self._last_frame_time = Date.now();
-        
-        // start animation
-        window.requestAnimationFrame(self._callback);
-    };
-    
-    self._callback = function() {
-        var current_frame_time = Date.now();
-        var step = self._step_size * (current_frame_time - self._last_frame_time);
-        
-        // increase value
-        self._current_value += step;
-        
-        // set last frame time
-        self._last_frame_time = current_frame_time;
-        
-        // set target value
-        if (self._current_value < self._end_value) {
-            self._target.html(Math.round(self._current_value));
-        
-            // continue animation
-            window.requestAnimationFrame(self._callback);
-            
-        } else {
-            self._target.html(Math.round(self._end_value)); 
-        }
-    };
-    
-    // finalize object
-    self._init();
+	var self = this;
+	
+	self._target = target;
+	self._start_value = start_value;
+	self._end_value = end_value;
+	self._duration = duration;
+	self._current_value = start_value;
+	self._total_value = null;
+	self._step_size = null;
+	self._last_frame_time = null;
+		
+	self._init = function() {
+		// pre-calculate values to minimize operations later
+		self._total_value = self._end_value - self._start_value;
+		self._step_size = self._total_value / self._duration;
+		
+		// initial frame time
+		//Here
+		self._last_frame_time = Date.now();
+		
+		// start animation
+		window.requestAnimationFrame(self._callback);
+	};
+	
+	self._callback = function() {
+		var current_frame_time = Date.now();
+		var step = self._step_size * (current_frame_time - self._last_frame_time);
+		
+		// increase value
+		self._current_value += step;
+		
+		// set last frame time
+		self._last_frame_time = current_frame_time;
+		
+		// set target value
+		if (self._current_value < self._end_value) {
+			self._target.html(Math.round(self._current_value));
+		
+			// continue animation
+			window.requestAnimationFrame(self._callback);
+			
+		} else {
+			self._target.html(Math.round(self._end_value)); 
+		}
+	};
+	
+	// finalize object
+	self._init();
 }
 
 /**
@@ -103,20 +103,20 @@ Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
 
-    var is_position = true; 
-    var position = $('section#methodology').offset().top - 400;
+	var is_position = true; 
+	var position = $('section#methodology').offset().top - 400;
 	var methodology_links = $('section#methodology li.stock.feature');
 
-    // Function for displaying tween animation triggered by window position
-    $(window).scroll(function (event) {
-        if($(window).scrollTop() >= position && is_position) {
-            is_position = false;
-        	methodology_links.each(function() { 
-                var end_value = parseInt($(this).find('p').eq(0).text());
-        		new Tween($(this).find('p').eq(0), 0, end_value, 2000);
-    	   });
-        }  
-    });
+	// Function for displaying tween animation triggered by window position
+	$(window).scroll(function (event) {
+		if($(window).scrollTop() >= position && is_position) {
+			is_position = false;
+			methodology_links.each(function() { 
+				var end_value = parseInt($(this).find('p').eq(0).text());
+				new Tween($(this).find('p').eq(0), 0, end_value, 2000);
+		   });
+		}  
+	});
 };
 
 
