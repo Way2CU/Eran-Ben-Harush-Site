2670b2a4c2748e7d3e88ac46f518a7fc9641d6d0
talrez11 <tal@way2cu.com>
2015-12-14 12:37:18 +0300

    Script: Fixed indentation

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -63,37 +63,35 @@ function animate(container,container_elements,trigger_element,delay_time) {
 	/*
 	 * object initialization 
 	 */
-	 self._init = function() {
-        // connect signals
-        $(window).on('scroll', self.handle_scroll);
+	self._init = function() {
+        		// connect signals
+		$(window).on('scroll', self.handle_scroll);
 
-        // Find all container elements
-        self.container_elements = self.container.find(container_elements);
+       	 	// Find all container elements
+        		self.container_elements = self.container.find(container_elements);
 
 	 }
 
-	 self.handle_scroll = function(event) {
-	 	var over_position = $(window).scrollTop() >= self.position;
+ 	self.handle_scroll = function(event) {
+ 		var over_position = $(window).scrollTop() >= self.position;
 
-        if (over_position && !self.active) {
-
+        		if (over_position && !self.active) {
 			self.container_elements.each(function(index) {
-		        var item = self.container_elements.eq(index);
-			        if(!item.hasClass('active')){
-			        setTimeout(function() {
-		                self.handle_active(item);
+	       	 	var item = self.container_elements.eq(index);
+		        	if(!item.hasClass('active')){
+		        		setTimeout(function() {
+	                		self.handle_active(item);
 			        }, self.delay_time + (index * self.delay_time));
 			    }
 			});
 
-            self.active = true;
+            		self.active = true;
 
-        } else if (!over_position && self.active) {
-            clearTimeout();
-            self.active = false;
-        }
-
-	 }
+	        	} else if (!over_position && self.active) {
+	            		clearTimeout();
+	           		 self.active = false;
+	        	}
+ 	}
 
 	 self.handle_active = function(item) {
 	 	item.addClass('active');
@@ -101,7 +99,6 @@ function animate(container,container_elements,trigger_element,delay_time) {
 
 	// finish object initialization
 	self._init();	 
-
  }
 
 function Tween(target, start_value, end_value, duration) {
